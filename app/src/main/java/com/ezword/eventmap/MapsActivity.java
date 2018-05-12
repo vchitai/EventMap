8962ab3eefe0dea9dec397a36a7c30de441e56c3
vchitai <chitai.vct@gmail.com>
2018-05-12 16:57:27 +0700

    Add Search Function

--- app/src/main/java/com/ezword/eventmap/MapsActivity.java
+++ app/src/main/java/com/ezword/eventmap/MapsActivity.java
@@ -1,8 +1,14 @@
 package com.ezword.eventmap;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +19,7 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -61,5 +68,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
             }
         });
+
+        Button button1 = findViewById(R.id.button1);
+        final EditText searchview = findViewById(R.id.searchView1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                String g = searchview.getText().toString();
+
+                Geocoder geocoder = new Geocoder(getBaseContext());
+                List<Address> addresses = null;
+
+                try {
+                    // Getting a maximum of 3 Address that matches the input
+                    // text
+                    addresses = geocoder.getFromLocationName(g, 3);
+                    if (addresses != null && !addresses.equals(""))
+                        search(addresses);
+
+                } catch (Exception e) {
+
+                }
+
+            }
+        });
+    }
+
+    private void search(List<Address> addresses) {
+        Address address = (Address) addresses.get(0);
+        double home_long = address.getLongitude();
+        double home_lat = address.getLatitude();
+        LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+
+        String addressText = String.format(
+                "%s, %s",
+                address.getMaxAddressLineIndex() > 0 ? address
+                        .getAddressLine(0) : "", address.getCountryName());
+
+        MarkerOptions markerOptions = new MarkerOptions();
+
+        markerOptions.position(latLng);
+        markerOptions.title(addressText);
+
+        mMap.clear();
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15));
+        TextView locationTv = findViewById(R.id.latlongLocation);
+        locationTv.setText("Latitude:" + address.getLatitude() + ", Longitude:"
+                + address.getLongitude());
+
     }
 }
